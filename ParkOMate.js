f4e9cb473070314ec49978efd27953796e513f17
MacnMan <134378411+MacnMan@users.noreply.github.com>
2025-05-21 12:34:26 +0530

    25MAY25

---
+++ ParkOMate.js
@@ -0,0 +1,39 @@
+/**
+ * LoRaWAN Decoder for parkomate 
+ * Macnman Technologies Pvt.Ltd
+ * Writen By : MACNMAN
+ * 01da00000004000b000f00060000682c5ed7
+*/
+// const data =[0x01, 0x00, 0x07, 0xD7, 0x7D, 0x43, 0x6A, 0x07, 0x53, 0xBF, 0x42, 0xEB, 0x07, 0x35, 0x52, 0x42, 0xEB, 0x0A, 0x07, 0x42, 0x4D, 0x42, 0x48, 0x00, 0x00, 0x7A, 0x9C];
+// const data = [0x04, 0x01, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x05, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0F, 0x29, 0x66, 0xCC, 0x74, 0x21];
+// console.log(Decoder(data));
+// decoding uploaded data
+function decodeUplink(input) {
+    return {
+        data: {
+            Payload: Decoder(input.bytes, input.port),
+        },
+        warnings: [],
+        errors: []
+    };
+}
+
+function Decoder(bytes, port) {
+    var payloadData = {
+        // "event": 0,
+        // "oem": "MACNMAN",
+        // "device": "ParkOMate_LWAN_V1.0",
+    }
+    var dataIndex = 0;
+    payloadData.xOffset = (bytes[dataIndex++] << 8) + bytes[dataIndex++];
+    payloadData.yOffset = (bytes[dataIndex++] << 8) + bytes[dataIndex++];   
+    payloadData.zOffset = (bytes[dataIndex++] << 8) + bytes[dataIndex++];
+    payloadData.xData = (bytes[dataIndex++] << 8) + bytes[dataIndex++];
+    payloadData.yData = (bytes[dataIndex++] << 8) + bytes[dataIndex++];
+    payloadData.zData = (bytes[dataIndex++] << 8) + bytes[dataIndex++]; 
+    payloadData.xDiff = payloadData.xData - payloadData.xOffset;
+    payloadData.yDiff = payloadData.yData - payloadData.yOffset;    
+    payloadData.zDiff = payloadData.zData - payloadData.zOffset;
+    payloadData.Systimestamp = (bytes[dataIndex++] << 24) + (bytes[dataIndex++] << 16) + (bytes[dataIndex++] << 8) + bytes[dataIndex++];
+    return payloadData;
+}
